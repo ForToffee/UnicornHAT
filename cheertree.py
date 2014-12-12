830ffa440726e13d5b74d111ef156689d37f4a46
ForToffee <github@fortoffee.org.uk>
2014-12-12 12:43:50 +0000

    added tree modes

--- cheertree.py
+++ cheertree.py
@@ -16,7 +16,15 @@ pattern =        [[3,0],
 		  [3,6],
 		  [3,7]]
 
-maxPixels = len(pattern)
+lights = [[3,0],[1,2],[5,2],[3,4],[0,5],[6,5]]
+mode = 1	#0=all pixels, 1=lights list only, 2=star only
+
+if mode == 0:
+    maxPixels = len(pattern)
+elif mode == 1:
+    maxPixels = len(lights)
+elif mode == 2:
+    maxPixels = 1
 
 namesToRGB = {'red': [0xFF, 0, 0],
                 'green': [0, 0x80, 0],
@@ -34,13 +42,23 @@ namesToRGB = {'red': [0xFF, 0, 0],
 
 #retrieve and load the JSON data into a JSON object
 def getJSON(url):
-    jsonFeed = urllib2.urlopen(urlRoot + url)
-    feedData = jsonFeed.read()
-    #print feedData
-    jsonFeed.close()
-
-    data = json.loads(feedData)
-    return data
+	try:
+		jsonFeed = urllib2.urlopen(urlRoot + url)
+		feedData = jsonFeed.read()
+		#print feedData
+		jsonFeed.close()
+		data = json.loads(feedData)
+		return data
+	except urllib2.HTTPError, e:
+		print 'HTTPError = ' + str(e.code)
+	except urllib2.URLError, e:
+		print 'URLError = ' + str(e.reason)
+	except httplib.HTTPException, e:
+		print 'HTTPException'
+	except Exception:
+		import traceback
+		print 'generic exception: ' + traceback.format_exc()
+	
 
 #use the JSON object to identify the colour in use,
 #update the last entry_id processed
@@ -50,6 +68,8 @@ def parseColour(feedItem):
 
     for name in namesToRGB.keys():
         if feedItem["field1"] == name:
+            if mode != 0 and name == 'green':	#ignore green when in lights or star mode
+                break 
             pixels.insert(0, namesToRGB[name])    #add the colour to the head
             break
 
@@ -64,9 +84,16 @@ def showPixels():
     global pixels
     index = 0
 
+    if mode != 0:		#green tree
+        for coords in pattern:
+            UH.set_pixel(coords[0],coords[1], 0, 0xFF, 0)
+	
     for pixel in pixels:
-        coords = pattern[index]
-	UH.set_pixel(coords[0],coords[1], pixel[0], pixel[1], pixel[2])
+        if mode == 0:
+            coords = pattern[index]
+        else:
+            coords = lights[index]
+        UH.set_pixel(coords[0],coords[1], pixel[0], pixel[1], pixel[2])
         index += 1
         if index >= maxPixels:
             #print index, maxPixels
@@ -83,12 +110,14 @@ def showColour(c):
 #main program
 
 UH.rotation(180)
+UH.brightness(0.4)
 
 #process the currently available list of colours
-data = getJSON("feed.json")
-for feedItem in data["feeds"]:
-    parseColour(feedItem)
-showPixels()
+if mode != 2:
+    data = getJSON("feed.json")
+    for feedItem in data["feeds"]:
+        parseColour(feedItem)
+    showPixels()
 
 #check for new colour requests
 while True:
@@ -97,8 +126,9 @@ while True:
     if getEntryID(data) > lastID:   #Has this entry_id been processed before?
         parseColour(data)
         showColour(pixels[0])
-        time.sleep(5)
-        showPixels()
+        if mode != 2:
+            time.sleep(5)
+            showPixels()
     else:
         time.sleep(refresh)
 
